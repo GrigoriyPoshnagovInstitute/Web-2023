445edcda3ef2f077da4e0afe47ec6bc53ec7c58f
grigoriy_poshnagov <poshnagovgrego@gmail.com>
2023-03-31 09:53:45 +0300

    Первый блок с блогами работает! 5 лаба

--- WEB/GO/cmd/blog/handlers.go
+++ WEB/GO/cmd/blog/handlers.go
@@ -6,30 +6,64 @@ import (
 	"net/http"
 )
 
-type indexPageData struct {
-	Title    string
-	Subtitle string
+type indexPage struct {
+	Title         string
+	FeaturedPosts []featuredPostData
+}
+
+type featuredPostData struct {
+	Span        string
+	SpanClass   string
+	Title       string
+	Subtitle    string
+	ImgModifier string
+	Author      string
+	AuthorImg   string
+	PublishDate string
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("pages/index.html")
+	ts, err := template.ParseFiles("pages/index.html") // Главная страница блога
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500) // В случае ошибки парсинга - возвращаем 500
+		log.Println(err.Error())                    // Используем стандартный логгер для вывода ошбики в консоль
+		return                                      // Не забываем завершить выполнение ф-ии
+	}
+
+	data := indexPage{
+		Title:         "Travel blog",
+		FeaturedPosts: featuredPosts(),
+	}
+
+	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
 	if err != nil {
 		http.Error(w, "Internal Server Error", 500)
-		log.Println(err)
+		log.Println(err.Error())
 		return
 	}
+}
 
-	data := indexPageData{
-		Title:    "Blog for travelling",
-		Subtitle: "My best blog",
+func featuredPosts() []featuredPostData {
+	return []featuredPostData{
+		{
+			Span:        "ADVENTURE",
+			SpanClass:   "featured-post__splash",
+			Title:       "The Road Ahead",
+			Subtitle:    "The road ahead might be paved - it might not be.",
+			ImgModifier: "featured-post__background_the-road-ahead",
+			Author:      "Mat Vogels",
+			AuthorImg:   "static/img/Mat-Vogels.png",
+			PublishDate: "September 25, 2015",
+		},
+		{
+			Span:        "",
+			SpanClass:   "",
+			Title:       "From Top Down",
+			Subtitle:    "Once a year, go someplace you’ve never been before.",
+			ImgModifier: "featured-post__background_from-top-to-down",
+			Author:      "William Wong",
+			AuthorImg:   "static/img/William-Wong.png",
+			PublishDate: "September 25, 2015",
+		},
 	}
-
-	err = ts.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Errors", 500)
-		log.Println(err)
-		return
-	}
-	log.Println("Request completed succesfuly")
-
 }
