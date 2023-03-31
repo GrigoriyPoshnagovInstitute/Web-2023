1f67b5da56a70897a45438ca8afef767f216828d
grigoriy_poshnagov <poshnagovgrego@gmail.com>
2023-03-31 11:03:37 +0300

    Почти сделал 5 лабу

--- WEB/GO/cmd/blog/handlers.go
+++ WEB/GO/cmd/blog/handlers.go
@@ -7,8 +7,9 @@ import (
 )
 
 type indexPage struct {
-	Title         string
-	FeaturedPosts []featuredPostData
+	Title           string
+	FeaturedPosts   []featuredPostData
+	MostRecentPosts []mostRecentPostData
 }
 
 type featuredPostData struct {
@@ -17,6 +18,16 @@ type featuredPostData struct {
 	Title       string
 	Subtitle    string
 	ImgModifier string
+	Author      string
+	AuthorImg   string
+	PublishDate string
+}
+
+type mostRecentPostData struct {
+	Img         string
+	Title       string
+	Subtitle    string
+	ImgDir      string
 	Author      string
 	AuthorImg   string
 	PublishDate string
@@ -31,8 +42,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	data := indexPage{
-		Title:         "Travel blog",
-		FeaturedPosts: featuredPosts(),
+		Title:           "Travel blog",
+		FeaturedPosts:   featuredPosts(),
+		MostRecentPosts: mostRecentPosts(),
 	}
 
 	err = ts.Execute(w, data) // Заставляем шаблонизатор вывести шаблон в тело ответа
@@ -64,6 +76,65 @@ func featuredPosts() []featuredPostData {
 			Author:      "William Wong",
 			AuthorImg:   "static/img/William-Wong.png",
 			PublishDate: "September 25, 2015",
+		},
+	}
+}
+
+func mostRecentPosts() []mostRecentPostData {
+	return []mostRecentPostData{
+		{
+			Img:         "air_baloons.png",
+			Title:       "Still Standing Tall",
+			Subtitle:    "Life begins at the end of your comfort zone.",
+			ImgDir:      "static/img/air-baloons.png",
+			Author:      "William-Wong",
+			AuthorImg:   "static/img/William-Wong.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Img:         "bridge.png",
+			Title:       "Sunny Side Up",
+			Subtitle:    "No place is ever as bad as they tell you it’s going to be.",
+			ImgDir:      "static/img/bridge.png",
+			Author:      "Mat-Vogels",
+			AuthorImg:   "static/img/Mat-Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Img:         "sunset.png",
+			Title:       "Water Falls",
+			Subtitle:    "We travel not to escape life, but for life not to escape us.",
+			ImgDir:      "static/img/sunset.png",
+			Author:      "Mat-Vogels",
+			AuthorImg:   "static/img/Mat-Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Img:         "tide.png",
+			Title:       "Through the Mist",
+			Subtitle:    "Travel makes you see what a tiny place you occupy in the world.",
+			ImgDir:      "static/img/tide.png",
+			Author:      "William-Wong",
+			AuthorImg:   "static/img/William-Wong.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Img:         "mist.png",
+			Title:       "Awaken Early",
+			Subtitle:    "Not all those who wander are lost.",
+			ImgDir:      "static/img/mist.png",
+			Author:      "Mat-Vogels",
+			AuthorImg:   "static/img/Mat-Vogels.png",
+			PublishDate: "9/25/2015",
+		},
+		{
+			Img:         "waterfall.png",
+			Title:       "Try it Always",
+			Subtitle:    "The world is a book, and those who do not travel read only one page.",
+			ImgDir:      "static/img/waterfall.png",
+			Author:      "Mat-Vogels",
+			AuthorImg:   "static/img/Mat-Vogels.png",
+			PublishDate: "9/25/2015",
 		},
 	}
 }
